b5d67693cea5fc492ee36df35b9b70fb136849ed
josubho <95227663+josubho@users.noreply.github.com>
2024-02-21 09:19:37 +0100

    Solution to module-two checkpoint-four

---
+++ checkpoint-four.py
@@ -0,0 +1,39 @@
+from decimal import Decimal
+import math
+
+#Exercise 1: Create a list, tuple, float, integer, decimal, and dictionary.
+my_list = [1, 3, 56, 2]
+my_tuple = (1, 4, 6, 8)
+float_number = 1.3
+integer_number = 34
+decimal = Decimal(2.58)
+my_dictionary = { "james" : 23, "bily" : 55, "isabelle" : 85}
+
+#Exercise 2: Round your float up.
+print(math.ceil(float_number))
+
+#Exercise 3: Get the square root of your float.
+print(math.sqrt(float_number))
+
+#Exercise 4: Select the first element from your dictionary.
+first_element = my_dictionary.values()[0]
+print(first_element)
+
+#Exercise 5: Select the second element from your tuple.
+print(my_tuple[1])
+
+#Exercise 6: Add an element to the end of your list.
+print(my_list.append(5))
+
+#Exercise 7: Replace the first element in your list.
+my_list[0] = 99
+print(my_list)
+
+#Exercise 8: Sort your list alphabetically.
+letter_list = ["g", "r", "z", "a"]
+letter_list.sort()
+print(letter_list)
+
+#Exercise 9: Use reassignment to add an element to your tuple.
+new_tupple = my_tuple + (15, )
+print(new_tupple)
